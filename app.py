9c84c5cf56f0984318ec6929bb715ff1baa2b944
Amin Al-Ait <AminAlAit@outlook.com>
2024-03-02 22:22:41 +0000

    quick fix

--- app.py
+++ app.py
@@ -30,9 +30,10 @@ import streamlit as st
 from st_pages import Page, add_page_title, show_pages
 show_pages(
     [
-        # Page("pages/0_Base.py",         "Base",         ""),
-        Page("pages/1_Patterns.py",       "Patterns",     ""),
-        # Page("pages/2_Experimental.py", "Experimental", ""),
+        # Page("pages/0_Base.py",          "Base",         ""),
+        # Page("pages/1_Patterns.py",      "Patterns",     ""),
+        Page("pages/0_Base.py",           "Patterns",     ""),
+        # Page("pages/2_Experimental.py",  "Experimental", ""),
         Page("pages/9_Contact.py",        "Contact",      "")
     ]
 )
