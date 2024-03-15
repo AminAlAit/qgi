9081417df0e4bdea4df11ec5256ffb6b4a471114
Amin Al-Ait <AminAlAit@outlook.com>
2024-03-15 01:28:32 +0000

    quick fix

--- constant/constant.py
+++ constant/constant.py
@@ -1,3 +1,3 @@
 OLD_PPR_PATH                  = r"data/ppr/old_ppr.csv"
 NEW_PPR_PATH                  = r"data/ppr/new_ppr.csv"
-ALL_COUNTRIES_EVENTS_CSV_PATH = r"/workspaces/qgi/data/events/All Countries.csv"
+ALL_COUNTRIES_EVENTS_CSV_PATH = r"data/events/All Countries.csv"
