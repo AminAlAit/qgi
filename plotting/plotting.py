eb012441dda882464ba9e68ad7a5286b8cd4a6fa
Amin Al-Ait <AminAlAit@outlook.com>
2024-04-18 21:23:01 +0000

    testing timeline 5

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -570,6 +570,9 @@ def display_timeline(five_params, country_a, start_year_a, country_b, start_year
 def get_events(country_a, year_range_a, country_b, year_range_b):
     EVENTS_CSVS_SOURCE_PATH = r"/workspaces/qgi/data/events/"
 
+    st.write(country_a)
+    st.write(country_b)
+
     st.write(EVENTS_CSVS_SOURCE_PATH, country_a + ".csv")
     st.write(EVENTS_CSVS_SOURCE_PATH, country_b + ".csv")
 
